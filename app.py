bb38d581d602f30fdc2d457ed9f1c3c623ebcc92
FisshKaa <72679512+FisshKaa@users.noreply.github.com>
2024-04-24 01:35:55 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='')
 
 @app.route('/recruto/', methods=['GET'])
 def hello_recruto():
